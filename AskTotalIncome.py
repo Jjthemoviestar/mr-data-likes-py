861f7d4dabbd58001223c433c9d56503ffc228be
Jjthemoviestar <Jjthemoviestar@gmail.com>
2018-10-16 15:58:46 -0400

    Add files via upload

--- AskTotalIncome.py
+++ AskTotalIncome.py
@@ -36,8 +36,18 @@ plt.ylabel('Loan Amount')
 plt.legend()
 plt.show()
 
-f1.loan_data['Income to Loan Ratio'] = totinc/(lnamt*1000) 
-f1.loan_data[:5]
+#this isn't right
+#I'm not creating a new column correctly, I need to use apply
+#can the calculation be done in a def and then that placed in the column?
+#e.g. f1.loan_data['Loan_Status_Num'] = f1.loan_data.apply(ln_stat_num, axis=1) 
+def inc_ln_ratio:
+    f1.loan_data['Income to Loan Ratio'] = totinc/(lnamt*1000) 
+        
+    
+inc_ln_ratio = f1.loan_data['Income to Loan Ratio'] = totinc/(lnamt*1000) 
+inc_ln_ratio[:5]
+
+print (f1.loan_data)
 
 """
 some of the data needs to be scrubbed, there are loans approved with no loan amount
